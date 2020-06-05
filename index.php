a595b31365e7323b78391d399e06b2d42f21a83b
CarmineV <carmine.vitale90@gmail.com>
2020-06-05 15:12:55 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP e OOP 2: Ereditarietà</title>
+</head>
+<body>
+    
+</body>
+</html>
